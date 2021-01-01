81a8e085045f3e76b0ee36b283181bf92bfce122
Sebastian <sebastian.calderon@sansano.usm.cl>
2020-12-31 21:05:00 -0300

    se mejoro la interfaz

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 
 import Background from './Background.js';
-import '../style/start.css';
-import TextField from '@material-ui/core/TextField';
-
-
-import DeleteIcon from '@material-ui/icons/Delete';
-import Button from '@material-ui/core/Button';
-import { Done } from "@material-ui/icons";
 import Todolist from "./Todolist.js";
   
 
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import '../style/start.css';
 
-
+import Typography from '@material-ui/core/Typography';
 
 import TextField from '@material-ui/core/TextField';
 
@@ -15,23 +15,40 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
-import { Create, Done } from "@material-ui/icons";
 
+import { Create, Done,Add} from "@material-ui/icons";
 
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import IconButton from '@material-ui/core/IconButton';
 
 
-
+import { green, pink } from '@material-ui/core/colors';
 import FolderIcon from '@material-ui/icons/Folder';
+import { createSvgIcon } from "@material-ui/core";
 
 
-function generate(element) {
-    return [0, 1, 2].map((value) =>
-      React.cloneElement(element, {
-        key: value,
-      }),
-    );
-  }
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    pink: {
+      color: theme.palette.getContrastText(pink[500]),
+      backgroundColor: pink[500],
+    },
+    green: {
+      color: '#fff',
+      backgroundColor: green[500],
+    },
+  });
+
+
+
 
 class Todolist extends Component{
     constructor(props){
@@ -70,13 +87,14 @@ class Todolist extends Component{
 
     render(){
 
-
+        const {classes} = this.props;
         return(
 
             <div id = "capaSup">
                 <div id="capaInf">
                     <div id = "textFill">
                             <TextField  
+                             style = {{width: 430}}
                             name = "text"
                             value = {this.state.text}
                             onChange = {this.handleChange}
@@ -85,43 +103,58 @@ class Todolist extends Component{
                                     style:{background:"white"},
                                 }}
                             />
-                                        <Button onClick = {this.addItem}
-                            variant="contained"
-                            color="secondary"
-                            startIcon={<Done />}
-                        >
-                        Agregar
-                        </Button >
+                           
+                                <Button className = {classes.pink}  onClick = {this.addItem}>
+                             <Add  size = "medium"
+                                ></Add>
+                             </Button>
+                            
+                     
+                         
+
+
+
                     </div>
                     <div id = "listItem">
-                        <List dense={this.state.items}>
+                        <List dense={this.state.items} disablePadding = "true">
                         {this.state.items.map(x =>
                             <ListItem >
                             <ListItemAvatar>
-                                <Avatar>
-                                <Create/>
-                                </Avatar>
+                            <Avatar className={classes.pink}>
+                             <AssignmentIcon />
+
+                        </Avatar>
                             </ListItemAvatar>
-                            <ListItemText fullWidth = "true" 
-                                primary={x}
+                            <ListItemText 
+                             disableTypography
+                        primary={<Typography  type="body2" style={{  fontSize:"large", color: '#FFFFFF' }}>{x}</Typography>}
                             />
+
+
                             <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="delete">
-                                <DeleteIcon />
-                                </IconButton>
+
+                            
+                            <IconButton >
+                                <Avatar className={classes.pink}>
+                            <DeleteIcon ></DeleteIcon>
+    
+                            </Avatar>
+                            </IconButton>
                             </ListItemSecondaryAction>
                             </ListItem>,
                         )}
                         </List>
                     </div>
-
+                        <div id = "final">
                     <Button 
                             variant="contained"
                             color="secondary"
                             startIcon={<Done />}
+                            fullWidth = "true"
                         >
                         Empezar
-                        </Button >
+                 </Button >
+                 </div>
        
         </div>
         
@@ -137,4 +170,4 @@ class Todolist extends Component{
 }
 
 
-export default Todolist;
+export default withStyles(styles, { withTheme: true })(Todolist);

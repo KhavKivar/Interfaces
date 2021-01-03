50a09b3d51c3ffcc3b4dcc69e01185e988b278bc
dmoragaa <diego.moragaa@sansano.usm.cl>
2021-01-02 23:34:12 -0300

    vista de calendario historial

---
+++ src/components/Calendar.js
@@ -0,0 +1,88 @@
+import { createMuiTheme, withStyles } from '@material-ui/core'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import '../style/calendar.css';
+import React, { Component } from 'react'
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    pink: {
+        '&:hover': {
+            backgroundColor: "white",
+            color: '#FFF'
+        },
+
+      color: theme.palette.getContrastText("#f50057"),
+      backgroundColor: "white",
+    },
+  });
+
+  const theme = createMuiTheme({
+    overrides: {
+        MuiButton: {
+            label: {
+                color: " #DB524D"
+              },
+        },
+      },
+
+
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Oxygen',
+        'sans-serif',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+      ].join(','),
+    },
+  });
+
+class Calendar extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+
+    render(){
+        return (
+        <div>
+            <div id="cal">
+                <FullCalendar 
+                aspectRatio="1.8" 
+                defaultView="dayGridMonth" 
+                plugins={[ dayGridPlugin ]} 
+                themeSystem="theme"
+                firstDay="1"
+                locale="esLocale"
+                events={[ //eventos dummy por el momento    
+                    {
+                      id: 'a',
+                      title: 'Pomodoro 1',
+                      start: '2021-01-01'
+                    },
+                    {
+                        id: 'b',
+                        title: 'Pomodoro 2',
+                        start: '2021-01-01'
+                    }
+                  ]}
+                />
+                
+            </div>
+        </div>
+    )}
+}
+
+export default withStyles(styles, { withTheme: true })(Calendar);
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import Background from './Background.js';
-
 
 import Count from "./Count.js";
 import Todolist from "./Todolist.js";
 
   
 
-import { Done,Add} from "@material-ui/icons";
+import { Done, ArrowBack} from "@material-ui/icons";
 import Button from '@material-ui/core/Button';
 
 import { withStyles } from '@material-ui/core/styles';
-
+import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 
 import {ThemeProvider } from "@material-ui/core";
 
 import { createMuiTheme} from '@material-ui/core/styles';
+import Calendar from "./Calendar.js";
 
 
 
@@ -71,12 +71,14 @@ class Main extends Component{
     constructor(props){
         super(props);
         this.handle = this.handle.bind(this);
+        this.handleHist = this.handleHist.bind(this);
 
 
         this.TodolistRef = React.createRef();
 
         this.state = {
-            start:false
+            start:false,
+            hist:false,
         };
 
     }
@@ -88,10 +90,19 @@ class Main extends Component{
       })
 
       }
-    
 
-        
+    }
 
+    handleHist(){
+      if(this.state.hist){
+        this.setState({
+          hist:false
+        })
+      } else {
+        this.setState({
+          hist:true
+        })
+      }
     }
 
     render(){
@@ -102,25 +113,60 @@ class Main extends Component{
             <div>
             <Background></Background>
             <div id = "capaSup">
-                {this.state.start ? <Count></Count>: <Todolist ref={this.TodolistRef}></Todolist>}
-                {!this.state.start && 
+
+                {this.state.hist ? //if hist, mostramos el calendario y el btn volver
+                <Fragment> 
+                  <Calendar></Calendar>
+                  <div id="final">
+                    <Button 
+                      className = {classes.pink}
+                          onClick={this.handleHist}
+                          variant="contained"
+                          color="secondary"
+                          startIcon={<ArrowBack />}
+                          fullWidth = {true}
+                          size = "large"
+                      >
+                      Volver
+                    </Button > 
+                  </div>
+                </Fragment> : //si no, se muestra el contador, o el todolist
+                this.state.start ? <Count></Count>: <Todolist ref={this.TodolistRef}></Todolist>}
                     <div id = "final">
                     <ThemeProvider theme={theme}>
-
-                    <Button 
-                    className = {classes.pink}
-                        onClick={this.handle}
-                        variant="contained"
-                        color="secondary"
-                        startIcon={<Done />}
-                        fullWidth = {true}
-                        size = "large"
-                    >
-                    Empezar
-                    </Button >
+                    {this.state.hist ? null :
+                      <Fragment>
+                        <Button 
+                      className = {classes.pink}
+                          onClick={this.handle}
+                          variant="contained"
+                          color="secondary"
+                          startIcon={<Done />}
+                          fullWidth = {true}
+                          size = "large"
+                      >
+                      Empezar
+                      </Button >
+                      
+                      <div style={{marginTop:"5px "}}>
+                        <Button 
+                        className = {classes.pink}
+                            onClick={this.handleHist}
+                            variant="contained"
+                            color="secondary"
+                            startIcon={<EventAvailableIcon />}
+                            fullWidth = {true}
+                            size = "large"
+                        >
+                        Ver historial
+                        </Button >
+                      </div>
+                      </Fragment>
+                    }
                     </ThemeProvider>
-                    </div>}
-                
+                    
+                    </div>
+        
             </div>
            
            </div>
@@ -129,4 +175,4 @@ class Main extends Component{
 
 }
 
-export default withStyles(styles, { withTheme: true })(Main);
+export default withStyles(styles, { withTheme: false })(Main);
